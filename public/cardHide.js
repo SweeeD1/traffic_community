8ea3010fec5894a538c698e3edf306c34f3270fc
Andrew Bortniy <a.bortniy@gmail.com>
2024-04-19 20:26:50 +0300

    ura peremoga bude

--- public/cardHide.js
+++ public/cardHide.js
@@ -1,18 +1,23 @@
 window.addEventListener('resize', function() {
-  var parentWidth = document.querySelector('.dashboard_container-history_links').offsetWidth;
-  var cardContent = document.getElementById('cardContent');
-  var originalContent = cardContent.getAttribute('data-original-content');
-  
-  if (!originalContent) {
-    originalContent = cardContent.textContent;
-    cardContent.setAttribute('data-original-content', originalContent);
-  }
-  
-  if (parentWidth < 400) {
-    var firstFour = originalContent.substr(0, 4);
-    var lastFour = originalContent.substr(-4);
-    cardContent.textContent = firstFour + '...' + lastFour;
-  } else {
-    cardContent.textContent = originalContent; // Восстанавливаем исходное содержимое
-  }
+  // Получаем все элементы с классом "cardContent"
+  var cardContents = document.querySelectorAll('.cardContent');
+
+  // Проходимся по каждому элементу
+  cardContents.forEach(function(cardContent) {
+    var parentWidth = cardContent.closest('.dashboard_container-history_links').offsetWidth; // Получаем ширину родительского элемента
+    var originalContent = cardContent.getAttribute('data-original-content');
+    
+    if (!originalContent) {
+      originalContent = cardContent.textContent;
+      cardContent.setAttribute('data-original-content', originalContent);
+    }
+    
+    if (parentWidth < 400) {
+      var firstFour = originalContent.substr(0, 4);
+      var lastFour = originalContent.substr(-4);
+      cardContent.textContent = firstFour + '...' + lastFour;
+    } else {
+      cardContent.textContent = originalContent; // Восстанавливаем исходное содержимое
+    }
+  });
 });
